2ea3060537747582243168d075ec27c7740b5084
Yorik van Havre <yorik@uncreated.net>
2014-12-10 16:38:40 -0200

    Path: Added Path.insert() function

--- src/Mod/Path/App/AppPathPy.cpp
+++ src/Mod/Path/App/AppPathPy.cpp
@@ -28,18 +28,88 @@
 
 #include <Base/Console.h>
 #include <Base/VectorPy.h>
+#include <Base/FileInfo.h>
+#include <Base/Interpreter.h>
 #include <App/Document.h>
 #include <App/Application.h>
 
 #include "CommandPy.h"
 #include "PathPy.h"
+#include "Path.h"
 #include "FeaturePath.h"
 
 using namespace Path;
 
 
-static PyObject * 
-show(PyObject *self, PyObject *args)
+static PyObject * insert (PyObject *self, PyObject *args)
+{
+    char* Name;
+    const char* DocName;
+    char* Pre = "";
+    if (!PyArg_ParseTuple(args, "ets|s","utf-8",&Name,&DocName,&Pre))
+        return NULL;
+    std::string EncodedName = std::string(Name);
+    PyMem_Free(Name);
+
+    Base::FileInfo file(EncodedName.c_str());
+    if (!file.exists())
+        Py_Error(Base::BaseExceptionFreeCADError, "File doesn't exist");
+    App::Document *pcDoc = App::GetApplication().getDocument(DocName);
+    if (!pcDoc)
+        pcDoc = App::GetApplication().newDocument(DocName);
+
+    // read the gcode file
+    std::ifstream filestr(file.filePath().c_str());
+    std::stringstream buffer;
+    buffer << filestr.rdbuf();
+    std::string gcode = buffer.str();
+    if (strcmp(Pre, "") != 0) {
+        // use a python Preprocessor
+        // by calling its parse() function
+        std::string modpath = "PathScripts.";
+        modpath += Pre;
+        PyObject *pModule, *pFunc, *pName, *pInput, *pOutput, *pArgs;
+        // get the global interpreter lock otherwise the app may crash with the error
+        // 'PyThreadState_Get: no current thread' (see pystate.c)
+        Base::PyGILStateLocker lock;
+        //Py_Initialize();
+        pName = PyString_FromString(modpath.c_str());
+        pModule = PyImport_Import(pName);
+        if (pModule != NULL) {
+            pFunc = PyObject_GetAttrString(pModule,"parse");
+            if (pFunc && PyCallable_Check(pFunc)) {
+                // function arguments must be packed into a tuple
+                pInput = PyString_FromString(gcode.c_str());
+                pArgs = PyTuple_New(1);
+                PyTuple_SetItem(pArgs, 0, pInput);
+                pOutput = PyObject_CallObject(pFunc, pArgs);
+                if (pOutput != NULL) {
+                    gcode = std::string(PyString_AsString(pOutput));
+                } else
+                    Py_Error(Base::BaseExceptionFreeCADError, "Preprocessor failed to parse the given file");
+            } else
+                Py_Error(Base::BaseExceptionFreeCADError, "Preprocessor does not contain a valid parse() function");
+        } else
+            Py_Error(Base::BaseExceptionFreeCADError, "Unable to load the given preprocessor");
+        Py_DECREF(pName);
+        Py_DECREF(pInput);
+        Py_DECREF(pOutput);
+        Py_DECREF(pModule);
+        Py_DECREF(pFunc);
+    }
+    std::cout << "after" << gcode << std::endl;
+    Toolpath path;
+    path.setFromGCode(gcode);
+
+    Path::Feature *object = static_cast<Path::Feature *>(pcDoc->addObject("Path::Feature",file.fileNamePure().c_str()));
+    object->Path.setValue(path);
+    pcDoc->recompute();
+
+    Py_Return;
+}
+
+
+static PyObject * show (PyObject *self, PyObject *args)
 {
     PyObject *pcObj;
     if (!PyArg_ParseTuple(args, "O!", &(PathPy::Type), &pcObj))     // convert args: Python->C
@@ -67,7 +137,9 @@ show(PyObject *self, PyObject *args)
 
 /* registration table  */
 struct PyMethodDef Path_methods[] = {
+    {"insert"     ,insert    ,METH_VARARGS,
+     "insert(filename,document,[preprocessor]): Imports a gcode file into the given document, optionally passing it through a pre-processor"},
     {"show"       ,show      ,METH_VARARGS,
-     "show(path) -- Add the path to the active document or create one if no document exists."},
+     "show(path): Add the path to the active document or create one if no document exists."},
     {NULL, NULL}        /* end of table marker */
 };
--- src/Mod/Path/PathScripts/pre_example.py
+++ src/Mod/Path/PathScripts/pre_example.py
@@ -1,9 +1,57 @@
-# example parser that works with griffin.ncc. It simply parses line over line,
-# and if a line doesn't start with a G or M command, the last one is repeated,
-# before the line. ensuring that all lines start with a G or M command.
+#***************************************************************************
+#*   (c) Yorik van Havre (yorik@uncreated.net) 2014                        *
+#*                                                                         *
+#*   This file is part of the FreeCAD CAx development system.              *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   FreeCAD is distributed in the hope that it will be useful,            *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Lesser General Public License for more details.                   *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with FreeCAD; if not, write to the Free Software        *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************/
+
+
+'''
+This is an example preprocessor file for the Path workbench. Its aim is to
+parse the contents of a given GCode file, and transform it to make it
+suitable for use in a Path object. This preprocessor, once placed in the
+appropriate PathScripts folder, can be used directly from inside FreeCAD,
+via the GUI importer or via python scripts with:
+
+import Path
+Path.import("/path/to/file.ncc","DocumentName","pre_example")
+
+It must contain at least a parse() function, that takes a string as 
+argument, which is the unmodified contents of the input GCode file, and 
+returns another string, that must respect the following 
+rules in order to be used by the Path workbench:
+
+- All lines must begin with a G or M command. Lines that don't respect that 
+  rule will be discarded on import.
+- Only one occurence of another letter can happen after a G  or M command. 
+  For example this is invalid:
+    G1 X1 Y2 
+    X2 Y3
+  You must write it like this: 
+    G1 X1 Y2 
+    G1 X2 Y3
+- Center coordinates (I,J) in G2 and G3 arcs are relative to the last point.
+'''
 
 def parse(inputstring):
-    "parse(inputstring): returns a parsed outputstring"
+    "parse(inputstring): returns a parsed output string"
+    print "preprocessing..."
     lines = inputstring.split("\n")
     output = ""
     lastcommand = None
@@ -16,7 +64,7 @@ def parse(inputstring):
             # remove line numbers
             l = l.split(" ",1)[1]
         if l[0] in ["(","%","#"]:
-            # discard comment and other non-gcode lines
+            # discard comment and other non strictly gcode lines
             continue
         if l[0].upper() in ["G","M"]:
             # found a G or M command: we store it
@@ -31,5 +79,8 @@ def parse(inputstring):
         elif lastcommand:
             # no G or M command: we repeat the last one
             output += lastcommand + " " + l + "\n"
-    return output            
+    print "done preprocessing."
+    return output
+
+print __name__ + " gcode preprocessor loaded."
 
