050221cf3affc902b269442f7b93d740efc33827
Daniel Falck <ddfalck@gmail.com>
2014-12-31 09:28:10 -0800

    arc direction and path order-still problems but better

--- src/Mod/Path/PathScripts/PathUtils.py
+++ src/Mod/Path/PathScripts/PathUtils.py
@@ -27,39 +27,25 @@ import Part
 from FreeCAD import Vector
 import FreeCADGui
 import math
+import DraftGeomUtils,DraftVecUtils
+import ArcDirection
 
 def segments(poly):
-    """A sequence of (x,y) numeric coordinates pairs """
+    ''' A sequence of (x,y) numeric coordinates pairs '''
     return zip(poly, poly[1:] + [poly[0]])
 
 def check_clockwise(poly):
     '''
-     a function for determining if the selected wire is clockwise or counter clockwise
-     based on selecting two consecutive edges
-     this will help place the tool path on the correct side of the wire
+     check_clockwise(poly) a function for returning a boolean if the selected wire is clockwise or counter clockwise
+     based on point order. poly = [(x1,y1),(x2,y2),(x3,y3)]
     '''
     clockwise = False
     if (sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in segments(poly))) < 0:
         clockwise = not clockwise
     return clockwise
 
-    '''
-    # example useage:
-    poly = [(2,2),(6,2),(6,6),(2,6)]
-    check_clockwise(poly)
-    False
-    '''
-    '''
-    vlist = []
-    for v in e.Vertexes:
-        vlist.append((v.Point.x,v.Point.y))
-
-    print vlist
-    check_clockwise(vlist)
-    '''
-
 def Sort2Edges(edgelist=[]):
-    '''simple function to reorder the start and end pts of two edges based on their selection order '''
+    '''Sort2Edges(edgelist=[]) simple function to reorder the start and end pts of two edges based on their selection order. Returns the list, the start point, and their common point, => edgelist, vertex, vertex'''
     if len(edgelist)>=2:
         vlist = []
         e0 = edgelist[0]
@@ -97,7 +83,7 @@ def Sort2Edges(edgelist=[]):
 def multiSelect():
     '''
     A function for selecting elements of an object for CNC path operations.
-    Select just a face, an edge, a vertex on the object, a point not on the object,
+    Select just a face, an edge,or two edges to indicate direction, a vertex on the object, a point not on the object,
     or some combination. Returns a dictionary.
     '''
     sel = FreeCADGui.Selection.getSelectionEx()
@@ -177,9 +163,10 @@ def multiSelect():
         else:
             for w in s.Object.Shape.Wires:
                 for e in  w.Edges:
-                    if e.isSame(edge):
-                        pathwire = w
-                        selItems['pathwire']  =pathwire
+                    if e.BoundBox.ZMax == e.BoundBox.ZMin: #if they are on same plane in Z as sel edge
+                        if e.isSame(edge):
+                            pathwire = w
+                            selItems['pathwire']  =pathwire
             selItems['edgelist'] =edgelist
 
     if not edges:
@@ -214,111 +201,69 @@ def convert(wire,Side,radius,clockwise=False,Z=0.0):
     # we use the OCC offset feature
         offset = wire.makeOffset(radius)
     elif Side == 'Right':
-        #wire.reverse()
         offset = wire.makeOffset(-radius)
     else:
-#        offset = wire.makeOffset(0.0) #tool is on the original profile ie engraving
-        offset = wire
+        offset = wire.makeOffset(0.0) #tool is on the original profile ie engraving
 
     if clockwise:
         revlist = []
         for edge in offset.Edges:
             revlist.append(edge)
         revlist.reverse()
-        
-        edgelist = []
-        #newlist = []
-        plast = offset.Edges[0].Vertexes[0].Point
-        for edge in revlist:
-            if isinstance(edge.Curve,Part.Circle):
-                start = edge.valueAt(edge.FirstParameter)
-                middle = edge.valueAt((edge.FirstParameter+edge.LastParameter)*0.5)
-                end = edge.valueAt(edge.LastParameter)
-                if start == plast:
-                    arc = Part.ArcOfCircle(start,middle,end)
-                else:
-                    arc = Part.ArcOfCircle(end,middle,start)
-                revedge=arc.toShape()
-                edgelist.append(revedge)
-                plast = end
-            elif isinstance(edge.Curve,Part.Line):
-                start = edge.Vertexes[0].Point
-                print start
-                end = edge.Vertexes[1].Point
-                if start == plast:
-                    revedge = Part.makeLine(start, end)
-                else:
-                    revedge = Part.makeLine(end, start)
-                edgelist.append(revedge)
-                plast = end
 
-        toolpath = Part.Wire(edgelist)
+        toolpath = DraftGeomUtils.sortEdgesOld(revlist)
 
     else:
+        newlist =[]
+        for edge in offset.Edges:
+            newlist.append(edge)
         FreeCAD.Console.PrintMessage('counter clockwise toolpath\n')
-        toolpath = offset.copy()
+        toolpath = newlist
     last = None
     output = ""
-    # we create the path from the offset shape
-    for edge in toolpath.Edges:
+    # create the path from the offset shape
+    for edge in toolpath:
         if not last:
-            # we set the base GO to our first point
+            #set the first point
             last = edge.Vertexes[0].Point
+            FreeCAD.Console.PrintMessage("last pt= " + str(last)+ "\n")
             output += "G1 X" + str(fmt(last.x)) + " Y" + str(fmt(last.y)) + " Z" + str(fmt(Z)) + "\n"
         if isinstance(edge.Curve,Part.Circle):
+            arcstartpt = edge.valueAt(edge.FirstParameter)
+            midpt = edge.valueAt((edge.FirstParameter+edge.LastParameter)*0.5)
+            arcendpt = edge.valueAt(edge.LastParameter)
+            arcchkpt=edge.valueAt(edge.LastParameter*.99)
 
-            point = edge.Vertexes[-1].Point
-            if point == last: # edges can come flipped
-                point = edge.Vertexes[0].Point
+            if DraftVecUtils.equals(last,arcstartpt):
+                startpt = arcstartpt
+                endpt = arcendpt
+            else:
+                startpt = arcendpt
+                endpt = arcstartpt
             center = edge.Curve.Center
             relcenter = center.sub(last)
-            # get the start,mid, and end pts
-            startpt = edge.valueAt(edge.FirstParameter)
-            midpt = edge.valueAt((edge.FirstParameter+edge.LastParameter)*0.5)
-            endpt = edge.valueAt(edge.LastParameter)
-
-#            v1 = last.sub(center)
-#            v2 = point.sub(center)
-#            if v1.cross(v2).z < 0:
-#                output += "G2"
-#            else:
-#                output += "G3"
-
+            FreeCAD.Console.PrintMessage("arc  startpt= " + str(startpt)+ "\n")
+            FreeCAD.Console.PrintMessage("arc  midpt= " + str(midpt)+ "\n")
+            FreeCAD.Console.PrintMessage("arc  endpt= " + str(endpt)+ "\n")
             arc_cw = check_clockwise([(startpt.x,startpt.y),(midpt.x,midpt.y),(endpt.x,endpt.y)])
-
-            if clockwise:
-                if edge.Orientation == 'Forward':
-                    if arc_cw:
-                        output += "G2"
-                    else:
-                        output += "G3"
-                else:
-                    if arc_cw:
-                        output += "G3"
-                    else:
-                        output += "G2"
+            FreeCAD.Console.PrintMessage("arc_cw="+ str(arc_cw)+"\n")
+            if arc_cw:
+                output += "G2"
             else:
-                if edge.Orientation == 'Forward':
-                    if arc_cw:
-                        output += "G3"
-                    else:
-                        output += "G2"
-                else:
-                    if arc_cw:
-                        output += "G2"
-                    else:
-                        output += "G3"
+                output += "G3"
 
-            output += " X" + str(fmt(point.x)) + " Y" + str(fmt(point.y)) + " Z" + str(fmt(Z))
+            output += " X" + str(fmt(endpt.x)) + " Y" + str(fmt(endpt.y)) + " Z" + str(fmt(Z))
             output += " I" + str(fmt(relcenter.x)) + " J" + str(fmt(relcenter.y)) + " K" + str(fmt(relcenter.z))
             output += "\n"
-            last = point
+            last = endpt
+            FreeCAD.Console.PrintMessage("last pt arc= " + str(last)+ "\n")
         else:
             point = edge.Vertexes[-1].Point
-            if point == last: # edges can come flipped
+            if DraftVecUtils.equals(point , last): # edges can come flipped
                 point = edge.Vertexes[0].Point
             output += "G1 X" + str(fmt(point.x)) + " Y" + str(fmt(point.y)) + " Z" + str(fmt(Z)) + "\n"
             last = point
+            FreeCAD.Console.PrintMessage("last pt line= " + str(last)+ "\n")
 
     return output
 
@@ -338,11 +283,10 @@ def approach(wire,Side,radius,clockwise,ZClearance,StepDown,ZStart, ZFinalDepth)
     first = offset.Edges[0].Vertexes[0].Point
     paths += "G0 X"+str(fmt(first.x))+"Y"+str(fmt(first.y))+"\n"
     ZCurrent = ZStart- StepDown
-    while ZCurrent >= ZFinalDepth:
+    while ZCurrent > ZFinalDepth:
         paths += convert(wire,Side,radius,clockwise,ZCurrent)
         ZCurrent = ZCurrent-abs(StepDown)
-    if ZCurrent <> ZFinalDepth:
-        paths += convert(wire,Side,radius,clockwise,ZFinalDepth)
+    paths += convert(wire,Side,radius,clockwise,ZFinalDepth)
     paths += "G0 Z" + str(ZClearance)
     return paths
 
