9837f3f4330368405247da83e60ba456e06fa4b3
brad <bradc@makerredux.com>
2015-01-08 23:21:17 -0600

    first pass cleanup
    *adds opensbp
    pre script splits operations
    post script not working
    
    consolidates examples.
    
    cleanup part 2
    
    ordered output
    Added a function to convert lines to dictionary.
    format numbers for desired precision.
    
    working on opensbp post.
    
    holding off on opensby post
    Fixing cmakelists

--- src/Mod/Path/PathScripts/post_example.py
+++ src/Mod/Path/PathScripts/post_example.py
@@ -39,51 +39,134 @@ another string, which is properly formatted GCode.
 
 import datetime
 now = datetime.datetime.now()
+from PathScripts import PostUtils
 
+OUTPUT_COMMENTS = True
+OUTPUT_HEADER = True
+OUTPUT_LINE_NUMBERS = False
+SHOW_EDITOR = True
+MODAL = False #if true commands are suppressed if the same as previous line.
+COMMAND_SPACE = ""
+LINENR = 100 #line number starting value
+
+
+#Preamble text will appear at the beginning of the GCODE output file.
+PREAMBLE = '''G17 G21 G90
+'''
+#Postamble text will appear following the last operation.
+POSTAMBLE = '''M05
+G00 X-1.0 Y1.0
+G17 G90
+M2
+'''
+ 
+#Pre operation text will be inserted before every operation
+PRE_OPERATION = '''
+ 
+'''
+ 
+#Post operation text will be inserted after every operation
+POST_OPERATION = '''
+
+'''
+
+#Tool Change commands will be inserted before a tool change
+TOOL_CHANGE = '''(A tool change)
+(happens here)
+'''
+
+def linenumber():
+    global LINENR
+    if OUTPUT_LINE_NUMBERS == True:
+        LINENR += 10 
+        return "N" + str(LINENR) + " "
+    return ""
 
 def parse(inputstring):
     "parse(inputstring): returns a parsed output string"
     print "postprocessing..."
     
     output = ""
-    
-    # write some stuff first
-    output += "N10 ;time:"+str(now)+"\n"
-    output += "N20 G17 G20 G80 G40 G90\n"
-    output += "N30 (Exported by FreeCAD)\n"
-    
-    linenr = 100
     lastcommand = None
+
+    params = ['X','Y','Z','A','B','I','J','K','F','S'] #This list control the order of parameters
+    
+    # write header
+    if OUTPUT_HEADER:
+        output += linenumber() + "(Exported by FreeCAD)\n"
+        output += linenumber() + "(Post Processor: " + __name__ +")\n"
+        output += linenumber() + "(Output Time:"+str(now)+")\n"
+    
+    #Write the preamble 
+    if OUTPUT_COMMENTS: output += linenumber() + "(begin preamble)\n"
+    for line in PREAMBLE.splitlines(True):
+        output += linenumber() + line
+
     # treat the input line by line
-    lines = inputstring.split("\n")
+    lines = inputstring.splitlines(True)
     for line in lines:
-        # split the G/M command from the arguments
-        if " " in line:
-            command,args = line.split(" ",1)
-        else:
-            # no space found, which means there are no arguments
-            command = line
-            args = ""
-        # add a line number
-        output += "N" + str(linenr) + " "
-        # only print the command if it is not the same as the last one
-        if command != lastcommand:
-            output += command + " "
-        output += args + "\n"
-        # increment the line number
-        linenr += 10
+        commandline = PostUtils.stringsplit(line)
+        outstring = []
+        command = commandline['command']
+        outstring.append(command) 
+        # if modal: only print the command if it is not the same as the last one
+        if MODAL == True:
+            if command == lastcommand:
+                outstring.pop(0) 
+
+        # Now add the remaining parameters in order
+        for param in params:
+            if commandline[param]: 
+                if param == 'F': 
+                    outstring.append(param + format(eval(commandline[param]), '.2f'))
+                else:
+                    outstring.append(param + format(eval(commandline[param]), '.4f'))
+        
+
         # store the latest command
         lastcommand = command
-        
-    # write some more stuff at the end
-    output += "N" + str(linenr) + " M05\n"
-    output += "N" + str(linenr + 10) + " M25\n"
-    output += "N" + str(linenr + 20) + " G00 X-1.0 Y1.0\n"
-    output += "N" + str(linenr + 30) + " G17 G80 G40 G90\n"
-    output += "N" + str(linenr + 40) + " M99\n"
-    
-    print "done postprocessing."
-    return output
 
+        # Check for Tool Change: 
+        if command == 'M6':
+            if OUTPUT_COMMENTS: output += "(begin toolchange)\n" 
+            for line in TOOL_CHANGE.splitlines(True):
+                output += linenumber() + line
+
+        if command == "message":
+          if OUTPUT_COMMENTS == False:
+            outstring = []
+          else:
+            outstring.pop(0) #remove the command
+
+        #prepend a line number and append a newline
+        if len(outstring) >= 1:
+            if OUTPUT_LINE_NUMBERS: 
+                outstring.insert(0,(linenumber()))
+            outstring.append("\n")
+
+            #append the line to the final output
+            for w in outstring:
+                output += w + COMMAND_SPACE
+    
+    # write some stuff at the end
+    if OUTPUT_COMMENTS: output += "(begin postamble)\n" 
+    for line in POSTAMBLE.splitlines(True):
+        output += linenumber() + line
+
+    if SHOW_EDITOR:
+        dia = PostUtils.GCodeEditorDialog()
+        dia.editor.setText(output)
+        result = dia.exec_()
+        if result:
+            final = dia.editor.toPlainText()
+        else:
+            final = output
+    else:
+        final = output
+      
+    print "done postprocessing."
+    return final
+   
+    
 print __name__ + " gcode postprocessor loaded."
 
