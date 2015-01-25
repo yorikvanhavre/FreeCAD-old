9a96b72200984498e8ba257f13187bc549cca755
Daniel Falck <ddfalck@gmail.com>
2015-01-24 18:54:23 -0800

    added pathplane and made fixture and pathprofile automatically populate or even create a project

--- src/Mod/Path/InitGui.py
+++ src/Mod/Path/InitGui.py
@@ -73,9 +73,10 @@ class PathWorkbench ( Workbench ):
         from PathScripts import PathProject
         from PathScripts import PathToolTableEdit
         from PathScripts import PathStock
+        from PathScripts import PathPlane
         # build commands list
         commands =["Path_Profile","Path_Pocket","Path_Drilling","Path_Dressup","Path_Hop","Path_Shape","Path_Copy",\
-        "Path_CompoundExtended","Path_Project","Path_Fixture","Path_ToolTableEdit","Path_Stock"]
+        "Path_CompoundExtended","Path_Project","Path_Plane","Path_Fixture","Path_ToolTableEdit","Path_Stock"]
 
         # Add commands to menu and toolbar
         def QT_TRANSLATE_NOOP(scope, text): return text
