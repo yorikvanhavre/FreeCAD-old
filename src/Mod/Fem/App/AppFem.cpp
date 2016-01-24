54dc32cea2aa4e9c96d63330c7d6e873458a3c41
wmayer <wmayer@users.sourceforge.net>
2016-01-24 02:34:12 +0100

    py3: ported Fem to python3

--- src/Mod/Fem/App/AppFem.cpp
+++ src/Mod/Fem/App/AppFem.cpp
@@ -60,7 +60,7 @@ extern PyObject* initModule();
 }
 
 /* Python entry */
-PyMODINIT_FUNC initFem()
+PyMOD_INIT_FUNC(Fem)
 {
     // load dependend module
     try {
@@ -69,7 +69,7 @@ PyMODINIT_FUNC initFem()
     }
     catch(const Base::Exception& e) {
         PyErr_SetString(PyExc_ImportError, e.what());
-        return;
+        PyMOD_Return(0);
     }
     PyObject* femModule = Fem::initModule();
     Base::Console().Log("Loading Fem module... done\n");
@@ -145,4 +145,6 @@ PyMODINIT_FUNC initFem()
     Fem::FemResultObjectPython      ::init();
     Fem::FemSolverObject            ::init();
     Fem::FemSolverObjectPython      ::init();
+
+    PyMOD_Return(femModule);
 }
