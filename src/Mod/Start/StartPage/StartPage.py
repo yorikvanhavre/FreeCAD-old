4a0b228268961eda14db9db34d6040db01447590
wmayer <wmayer@users.sourceforge.net>
2016-01-25 00:59:52 +0100

    py3: make most of the unit test to succeed

--- src/Mod/Start/StartPage/StartPage.py
+++ src/Mod/Start/StartPage/StartPage.py
@@ -23,9 +23,14 @@
 
 # This is the start page template
 
-import os,FreeCAD,FreeCADGui,tempfile,time,zipfile,urllib,re,cStringIO
+import os,FreeCAD,FreeCADGui,tempfile,time,zipfile,urllib,re
 from PySide import QtGui
 from xml.etree.ElementTree import parse
+
+try:
+    import io as cStringIO
+except:
+    import cStringIO
 
 FreeCADGui.addLanguagePath(":/translations")
 FreeCADGui.updateLocale()
@@ -602,7 +607,7 @@ def getFeed(url,numitems=3):
         resp += item['title']
         resp += '</a></li>'
     resp += '</ul>'
-    print resp
+    print (resp)
     return resp
 
 def getCustomBlocks():
